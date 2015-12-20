0b4d8be1489ad5c920c434bfef31fdc257760d86
yangjianzhou <659073645@qq.com>
2015-12-21 00:29:43 +0800

    add kfifo and list

---
+++ KFIFO/kfifo.h
@@ -0,0 +1,58 @@
+#ifndef __KFIFO_H
+#define __KFIFO_H	 
+#include "uart_command.h"
+
+enum {
+	HEAD1 = 0, //MUST 0
+	HEAD2,
+//	HEAD3,
+	LEN,
+	CMD,
+	DATA,
+	CHECK,
+#ifdef CMD_ADD_END
+	END,
+#endif	
+};
+
+#define uint16 unsigned short
+#define uint8  unsigned char
+//#define uint32_t unsigned int
+//512  256  128
+#define UART_KFIFO_LEN 1024
+//2048
+#define CAN_KFIFO_LEN 2048
+//1024
+
+//256
+#define CMD_LEN 64
+#define is_power_of_2(x) ((x) != 0 && (((x) & ((x) - 1)) == 0))
+#define min(a, b) (((a) < (b)) ? (a) : (b))
+
+struct kfifo {
+    void         *buffer;     /* the buffer holding the data */
+    unsigned int size;         /* the size of the allocated buffer */
+    unsigned int in;           /* data is added at offset (in % size) */
+    unsigned int out;          /* data is extracted from off. (out % size) */
+	  unsigned int lostBytes;
+	  unsigned int errorCmds;
+	  unsigned long acceptCmds;
+};
+
+struct kfifo* kfifo_init(void *buffer, unsigned int size, void *f_lock);
+struct kfifo *kfifo_alloc(unsigned int size, void *lock);
+void kfifo_free(struct kfifo *ring_buf);
+unsigned int kfifo_len(const struct kfifo *ring_buf);
+unsigned int kfifo_get(struct kfifo *ring_buf, void *buffer, unsigned int size);
+unsigned int kfifo_put(struct kfifo *ring_buf, void *buffer, unsigned int size);
+extern  void reset_fifo(struct kfifo *ring_buf);
+void kfifo_clean(struct kfifo *ring_buf);
+void printf_kfifo_info(struct kfifo *ring_buf);
+void* memcpy(void *des, const void *src, unsigned int n) ;
+extern struct kfifo* uart3_fifo;
+extern struct kfifo* uart6_fifo;
+extern struct kfifo* can1_fifo;
+extern struct kfifo* can2_fifo;
+
+#endif
+
---
+++ LIST/list.h
@@ -0,0 +1,122 @@
+#ifndef __LIST_H
+#define __LIST_H	 
+
+struct list_head{
+	struct list_head *next, *prev;
+};
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ * @ptr:	the pointer to the member.
+ * @type:	the type of the container struct this is embedded in.
+ * @member:	the name of the member within the struct.
+ *
+ */
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+//#define container_of(ptr, type, member) ({			\
+//	const typeof(((type *)0)->member) * __mptr = (ptr);	\
+//	(type *)((char *)__mptr - offsetof(type, member)); })
+
+#define container_of(ptr, type, member)  (type *)( (char *)ptr - offsetof(type,member) )
+	
+#define LIST_HEAD_INIT(name) { &(name), &(name) }
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+		
+	
+/**
+ * list_entry - get the struct for this entry
+ * @ptr:	the &struct list_head pointer.
+ * @type:	the type of the struct this is embedded in.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_entry(ptr, type, member) \
+	container_of(ptr, type, member)
+
+/**
+ * list_first_entry - get the first element from a list
+ * @ptr:	the list head to take the element from.
+ * @type:	the type of the struct this is embedded in.
+ * @member:	the name of the list_struct within the struct.
+ *
+ * Note, that list is expected to be not empty.
+ */
+#define list_first_entry(ptr, type, member) \
+	list_entry((ptr)->next, type, member)
+
+/**
+ * list_for_each	-	iterate over a list
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @head:	the head for your list.
+ */
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; pos != (head); pos = pos->next)
+	
+/**
+ * list_for_each_prev	-	iterate over a list backwards
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @head:	the head for your list.
+ */
+#define list_for_each_prev(pos, head) \
+	for (pos = (head)->prev; pos != (head); pos = pos->prev)
+
+
+/**
+ * list_for_each_safe - iterate over a list safe against removal of list entry
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ */
+#define list_for_each_safe(pos, n, head) \
+	for (pos = (head)->next, n = pos->next; pos != (head); \
+		pos = n, n = pos->next)
+		
+/**
+ * list_for_each_prev_safe - iterate over a list backwards safe against removal of list entry
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ */
+#define list_for_each_prev_safe(pos, n, head) \
+	for (pos = (head)->prev, n = pos->prev; \
+	     pos != (head); \
+	     pos = n, n = pos->prev)		
+
+/**
+ * list_for_each_entry	-	iterate over list of given type
+ * @pos:	the type * to use as a loop cursor.
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_for_each_entry(pos, head, member)				\
+	for (pos = list_entry((head)->next, typeof(*pos), member);	\
+	     &pos->member != (head); 	\
+	     pos = list_entry(pos->member.next, typeof(*pos), member))
+
+/**
+ * list_for_each_entry_safe - iterate over list of given type safe against removal of list entry
+ * @pos:	the type * to use as a loop cursor.
+ * @n:		another type * to use as temporary storage
+ * @head:	the head for your list.
+ * @member:	the name of the list_struct within the struct.
+ */
+#define list_for_each_entry_safe(pos, n, head, member)			\
+	for (pos = list_entry((head)->next, typeof(*pos), member),	\
+		n = list_entry(pos->member.next, typeof(*pos), member);	\
+	     &pos->member != (head); 					\
+	     pos = n, n = list_entry(n->member.next, typeof(*n), member))
+
+
+extern void INIT_LIST_HEAD(struct list_head *list);
+extern void list_add(struct list_head *new, struct list_head *head);
+extern void list_add_tail(struct list_head *new, struct list_head *head);
+extern void list_del(struct list_head *entry);
+extern void __list_del(struct list_head * prev, struct list_head * next);
+extern void __list_del_entry(struct list_head *entry);
+extern void list_move(struct list_head *list, struct list_head *head);	
+extern void list_move_tail(struct list_head *list,
+				  struct list_head *head);
+extern int list_empty(const struct list_head *head);
+#endif
+
